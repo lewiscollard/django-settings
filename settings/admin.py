5a4d05cbacc9afbef9019d210ae0f14a1a454e97
Lewis Collard <lewis.collard@onespacemedia.com>
2016-01-18 09:39:30 +0000

    Permit a non-existing SETTINGS_ADMINS
    This also fixes a bug in which permissions were reversed; get_form would exclude "name" and "key" if the user was *not* in SETTINGS_ADMINS.
    
    Also, add list_display so the key can be copied even if the user is not in SETTINGS_ADMINS.

--- settings/admin.py
+++ settings/admin.py
@@ -7,18 +7,28 @@ from .models import Setting
 class SettingAdmin(admin.ModelAdmin):
     prepopulated_fields = {'key': ['name']}
 
+    list_display = ["name", "key"]
+
     def get_form(self, request, obj=None, **kwargs):
-        if obj and request.user.username in settings.SETTINGS_ADMINS:
-            self.exclude = ['name', 'key']
-            self.prepopulated_fields = {}
+        if getattr(settings, "SETTINGS_ADMINS", None) is not None:
+            if obj and request.user.username not in settings.SETTINGS_ADMINS:
+                self.exclude = ['name', 'key']
+                self.prepopulated_fields = {}
 
         return super(SettingAdmin, self).get_form(request, obj, **kwargs)
 
     def has_add_permission(self, request):
-        return request.user.username in settings.SETTINGS_ADMINS
+        if getattr(settings, "SETTINGS_ADMINS", None) is not None:
+            return request.user.username in settings.SETTINGS_ADMINS
+        return super(SettingAdmin, self).has_add_permission(request)
 
     def has_delete_permission(self, request, obj=None):
-        return request.user.username in settings.SETTINGS_ADMINS
+        if getattr(settings, "SETTINGS_ADMINS", None) is not None:
+            return request.user.username in settings.SETTINGS_ADMINS
+        return super(SettingAdmin, self).has_delete_permission(request, obj)
+
+    def setting_name(self, obj):
+        return obj.name
 
     class Media:
         js = ("/static/settings/js/admin/fields.js",)
